3f250ccb9d049f373c8715678928000ca9f5447d
Patrick Wells <patrick.wells.95@gmail.com>
2024-10-08 11:40:15 -0500

    Commit some other changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,23 +3,8 @@ using Test
 using CSV
 using DataFrames
 
-test_data = CSV.read("./test_data.csv", DataFrame)
+test_path = "/Path/to/data"
+simulated_map = SimulatedMap(test_path, ["n", "1/r_meds", "z/r_meds"])
 
-@testset "LensKappa.jl" begin
-    @testset "Fit" begin
-        @testset "GEVFit" begin
-            f = fit(test_data[!,"kappa"], 100, :GEV)
-            @test isapprox(f.μ, -0.029, atol=1e-3)
-            @test isapprox(f.β, -3.342, atol=1e-3)
-            @test isapprox(f.ξ, 0.079, atol=1e-3)
-        end
 
-        @testset "GumbelFit" begin
-            f = fit(test_data[!,"kappa"], 100, :Gumbel)
-            @test isapprox(f.μ, -0.030, atol=1e-3)
-            @test isapprox(f.β, -3.345, atol=1e-3)
-        end
 
-    end
-    # Write your tests here.
-end
